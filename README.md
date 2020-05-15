4eb0a26769bc1f31f56d03bbdc639a33d074f971
Casper Smits <caspersmits11@outlook.com>
2020-05-16 00:27:38 +0200

    implementation audio mixing

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # text-generator
 Text generator based on samples with a given probability
+
+# Usage
+The program takes two arguments: the location of the task file, and the root directory of the audio
+files.
+```
+python generate.py /path/to/task_file.json /path/to/audio_files
+```
+
+# Installation instructions
+This section contains the installation instructions in order to run the program. I assume that 
+you have Python (version 3 or higher) and pip installed. 
+
+## ffmpeg
+If you are working with non-`.wav` files (such as `.mp3`), you need to install additional tools, 
+such as [ffmpeg](https://www.ffmpeg.org/). Check the [documentation of Pydub](https://github.com/jiaaro/pydub#dependencies) 
+for more information.
+
+### Windows
+Make sure that ffmpeg is installed and that it is added to your PATH system environment variable. 
+You can find a guide to install it on Windows [here](http://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/).
+
+**Note:** Sometimes a reboot of your system is required.  
+
+## Dependencies
+Upon up a python virtual environment and install the packages with the following command:
+```
+pip install -r requirements.txt
+```
--- generate.py
+++ generate.py
@@ -1,44 +1,162 @@
 import json
 import sys
 import random
+import os
+from pydub import AudioSegment
+import math
 
 
-def load_words(file_name):
+class Duration:
+    def __init__(self, seconds, segments):
+        self.seconds = seconds
+        self.segments = segments
+
+
+class Settings:
+    def __init__(self, seed, duration, breath_pause_length):
+        self.seed = seed
+        self.duration = duration
+        self.breath_pause_length = breath_pause_length
+
+
+class Segment:
+    def __init__(self, text, audio, weight, cool_down):
+        self.text = text
+        self.audio = audio
+        self.weight = weight
+        self.cool_down = cool_down
+
+
+class Task:
+    def __init__(self, segments, settings):
+        self.segments = segments
+        self.settings = settings
+        self.breath_pause = AudioSegment.silent(settings.breath_pause_length)
+        self.total_weight = 0
+        self.init()
+
+    def init(self):
+        random.seed(self.settings.seed)
+        for segment in self.segments:
+            self.total_weight += segment.weight
+
+    def get_random_segment(self):
+        return random.choice(self.segments)
+
+    def passed_max_duration(self, result):
+        if len(result.segments) > self.settings.duration.segments:
+            return True
+        if result.get_duration_in_seconds() > self.settings.duration.seconds:
+            return True
+        return False
+
+
+class Result:
+    def __init__(self):
+        self.text = None
+        self.audio = None
+        self.segments = []
+
+    def add_segment(self, segment):
+        self.segments.append(segment)
+        if self.text is None:
+            self.text = segment.text
+        else:
+            self.text += '. ' + segment.text
+        if self.audio is None:
+            self.audio = segment.audio
+        else:
+            self.audio += segment.audio
+
+    def add_breath_pause(self, breath_pause):
+        if self.audio is None:
+            self.audio = breath_pause
+        else:
+            self.audio += breath_pause
+
+    def get_duration_in_seconds(self):
+        if self.audio is None:
+            return 0
+        else:
+            return self.audio.duration_seconds
+
+
+def load_json(file_name):
     with open(file_name, 'r') as handle:
         raw = handle.read()
-        data = json.loads(raw)
-        # TODO: Check if key exists
-        return data['words']
+        return json.loads(raw)
 
 
-def parse_words(words):
-    """
-    Parses the example:
-    - Determines the whole count (and therefore probabilities)
-    """
-    total_count = 0
-    for _, weight in words.items():
-        total_count += weight
-    return [words, total_count]
+def load_segments(task_file, audio_folder):
+    segments = []
+    for segment in task_file["segments"]:
+        audio = AudioSegment.from_mp3(audio_folder + '/' + segment['audio'])
+        segments.append(Segment(segment['text'], audio, segment['weight'], segment['cool_down']))
+    return segments
 
 
-def generate_text(words, total_length, total_count):
-    text = ''
-    length = 0
-    while length < total_length:
-        threshold = random.randint(1, total_count)
-        candidate, weight = random.choice(list(words.items()))
-        if weight >= threshold:
-            text += candidate + '. '
-            length += 1
-    return text
+def load_settings(task_file):
+    settings = task_file['settings']
+    seed = settings['seed']
+    duration_in_seconds = settings['duration']['seconds']
+    duration_in_segments = settings['duration']['segments']
+    duration = Duration(duration_in_seconds, duration_in_segments)
+    breath_pause_length = settings['breath_pause_length']
+    return Settings(seed, duration, breath_pause_length)
+
+
+def load_task(task_file, audio_folder):
+    segments = load_segments(task_file, audio_folder)
+    settings = load_settings(task_file)
+    return Task(segments, settings)
+
+
+def get_percentage_done(result, settings):
+    return max(
+        len(result.segments) / settings.duration.segments,
+        result.get_duration_in_seconds() / settings.duration.seconds
+    )
+
+
+def execute_task(task):
+    result = Result()
+    failed_attempts = 0
+    report_at_next_percentage = 0
+    report_interval = 0.1
+
+    def honours_cool_down(_segment, _result, _relaxation):
+        if _segment.cool_down == 0:
+            return True
+        last_items = _result.segments[-(_segment.cool_down + _relaxation):]
+        return _segment not in last_items
+
+    while not task.passed_max_duration(result):
+        percentage_done = get_percentage_done(result, task.settings)
+        if percentage_done >= report_at_next_percentage:
+            print('Progress : ' + str(int(percentage_done * 100)))
+            report_at_next_percentage += report_interval
+
+        threshold = random.randint(1, task.total_weight)
+        segment = task.get_random_segment()
+        if segment.weight >= threshold and honours_cool_down(segment, result, math.floor(failed_attempts / 10)):
+            failed_attempts = 0
+            result.add_segment(segment)
+            result.add_breath_pause(task.breath_pause)
+        else:
+            failed_attempts += 1
+    print('Progress : 100')
+    return result
 
 
 if __name__ == '__main__':
-    file_name = sys.argv[1]
-    total_length = int(sys.argv[2])
-    words = load_words(file_name)
-    words, total_count = parse_words(words)
-    text = generate_text(words, total_length, total_count)
-    print(text)
+    # Load arguments
+    task_file_path = sys.argv[1]
+    audio_folder = sys.argv[2]
 
+    print('initialising...')
+    task = load_task(load_json(task_file_path), audio_folder)
+    print('generating...')
+    result = execute_task(task)
+
+    print(result.text)
+    result.audio.export('export.mp3')
